563dbd767e0719393f452eee355de11d62eaa3c9
Tanmoy Mazumdar <tmazumdar0@gmail.com>
2020-10-04 13:54:06 +0530

    In progress

--- app/src/main/java/com/example/rha/MainActivity.kt
+++ app/src/main/java/com/example/rha/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.rha
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myBtn=findViewById<Button>(R.id.my_button)
+
+        myBtn.setOnClickListener {
+            val intent=Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
